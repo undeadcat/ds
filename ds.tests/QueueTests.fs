6fd4d720e8e8cd37a422b3b806409da53793e71a
mskr <maxim.kropotov@gmail.com>
2014-08-06 18:44:46 +0400

    queues with suspended lists

--- ds.tests/QueueTests.fs
+++ ds.tests/QueueTests.fs
@@ -3,6 +3,7 @@ namespace ds.Tests
 open ds
 open System
 open NUnit.Framework
+open System.Diagnostics
 
 [<TestFixture>]
 type QueueTests() = 
@@ -30,4 +31,32 @@ type QueueTests() =
         let ls = List.init 10 (fun i ->i )
         let queue = List.fold (fun (q:Queue<int>) i -> q.Enqueue i) (new Queue<int>()) ls
         Assert.That (dump queue, Is.EqualTo ls)
+
+      
+    [<Test>]
+    member this.LoadTest() =
+
+        let doTest q queuer dequeuer title iters  =
+            let folder q i = 
+                 queuer q i |> dequeuer 
+            let sw = Stopwatch.StartNew()
+            let nums = [1..iters]
+            List.fold folder q nums|> ignore
+            sw.Stop()
+            printfn "%s %d %f" title iters (float sw.ElapsedTicks/float iters) 
+        let tests=[1000; 10000; 50000;]
+        let immutableTester = doTest (new Queue<int>()) (fun q i -> q.Enqueue i) (fun q-> q.Dequeue()|> snd ) "Immutable"
+        let mutableTester = doTest (new System.Collections.Generic.Queue<int>(List.max tests)) 
+                                (fun q i -> q.Enqueue i;q;) 
+                                (fun q -> ignore (q.Dequeue());q;) 
+                                "Mutable"
+        let stackTester = doTest [] 
+                                    (fun q i -> i::q) 
+                                    (fun q -> match q with
+                                                |[] -> raise (new Exception "Stack is empty")
+                                                | x::xs->xs )
+                                    "Stack"
+        List.iter (fun x -> immutableTester x; mutableTester x; stackTester x) tests
+         
+
    
--- ds/Queue.fs
+++ ds/Queue.fs
@@ -3,24 +3,30 @@ namespace ds
 open System
 open ds.Stream
 
-type Queue<'T> private (theFront:'T stream, fLength, theRear:'T stream, rLength) =
-    let (front:'T stream, frontLength, rear:'T stream, rearLength) = 
-        match rLength > fLength with
-            |true -> (Stream.Concat theFront (Stream.Reverse theRear), fLength + rLength, Stream.Empty<'T>(), 0)
-            |false -> (theFront, fLength, theRear, rLength)
+type Queue<'T> private (theWorking:'T list, theFront:'T list Lazy, fLength, theRear:'T list, rLength) =
+    
+    let ensureFront() = match rLength > fLength with
+                        |true -> (theWorking, lazy (theFront.Force() @ (List.rev theRear)), fLength + rLength, [], 0)
+                        |false -> (theWorking, theFront, fLength, theRear, rLength)
+    
+    let ensureWorking ((working, front:'T list Lazy, fl, rear, rl) as t) = 
+        if List.isEmpty working then (front.Force(), lazy([]), fl, rear, rl) 
+        else t 
+
+    let (working:'T list, front:'T list Lazy, frontLength, rear:'T list, rearLength) = ensureFront() |> ensureWorking
 
     member this.Dequeue() = 
-        match (front.Force()) with
-            | Nil -> (None, this)
-            | Cons(hd, tl) -> (Some(hd), new Queue<'T>(tl, frontLength-1, rear, rearLength))
+        match (working) with
+            | [] -> (None, this)
+            | hd::tl -> (Some(hd), new Queue<'T>(tl, front, frontLength - 1, rear, rearLength))
 
     member this.Enqueue value = 
-        new Queue<'T>(front, frontLength, lazy(Cons(value, rear)), rearLength+1)
+        new Queue<'T>(working, front, frontLength, value::rear, rearLength+1)
 
     new (vals:'T list) = 
-        new Queue<'T>(Stream.OfList vals, 
+        new Queue<'T>(vals, lazy([]), 
             List.length vals, 
-            Stream.Empty<'T>(), 0)
+            [], 0)
     new() = Queue<'T>(List.empty<'T>)
 
     
